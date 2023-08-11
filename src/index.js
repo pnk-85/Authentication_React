1bbd8116a1c96173624478f63d39221f0b40afe5
pnk-85 <pnkghogare27@gmail.com>
2023-08-11 22:32:04 +0530

    Managing token and change password

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { AuthcontextProvider } from './Store/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <AuthcontextProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </AuthcontextProvider>
 );
